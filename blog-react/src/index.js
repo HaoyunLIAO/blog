7944e0db3bcc7e73d7c9f571095eb222b631fb93
HaoyunLIAO <43165236+HaoyunLIAO@users.noreply.github.com>
2020-08-31 13:57:27 +0800

    添加页面

--- blog-react/src/index.js
+++ blog-react/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
+import store from '@/store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
